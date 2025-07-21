c2b742da87fbf0a64587cd2208745b22f791eba0
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-07-21 13:59:38 +0530

    Moved to backend folder

--- db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("DB connected Successfully");
-  }).catch((e) => {
-    console.log(e);
-});
